501133ebaf05c76fc36eaa4c232daf799d787dd9
maanas-k <SonalKatwala@gmail.com>
2021-02-03 18:45:08 +0530

    added a chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain1, chain2;
+var clog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +37,12 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    clog= new Log(150,120,70,PI/2)
+
     bird = new Bird(100,100);
+
+    chain1= new Chain(bird.body,clog.body);
+    chain2= new Chain(bird.body,pig3.body)
 
 }
 
@@ -61,4 +69,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    clog.display();
+    chain1.display();
+    chain2.display()
 }
